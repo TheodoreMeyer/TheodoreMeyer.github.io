e58ccfa6547c1375f077a907c67d9012fda292a3
TheodoreMeyer <thumpingtheo@gmail.com>
2026-09-17 10:48:12 -0400

    Fix deployment issue, add path aliases

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,40 @@
-import { defineConfig } from "vite";
+import {
+  defineConfig
+} from "vite";
+
 import react from "@vitejs/plugin-react";
 
+import path from "node:path";
+import {
+  fileURLToPath
+} from "node:url";
+
 import pagesPlugin from "./infrastructure/build/vite-plugin-pages.js";
+
+
+const __filename =
+    fileURLToPath(import.meta.url);
+
+const __dirname =
+    path.dirname(__filename);
+
 
 export default defineConfig({
   plugins: [
     pagesPlugin(),
     react()
   ],
+
+  resolve: {
+    alias: {
+      "#":
+          path.resolve(__dirname, "./infrastructure"),
+
+      "@":
+          path.resolve(__dirname, "./pages")
+    }
+  },
+
   build: {
     manifest: true
   }
